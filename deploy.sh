6dc749d3892f98e0f193c8a22985e5d8b72af05e
Amit Dewangan <amitmay94@gmail.com>
2020-02-06 11:13:34 +0530

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-docker build -t amitdewangan/multi-client -f ./client/Dockerfile ./client
-docker build -t amitdewangan/multi-server -f ./server/Dockerfile ./server
-docker build -t amitdewangan/multi-worker -f ./worker/Dockerfile ./worker
-docker push amitdewangan/multi-client
-docker push amitdewangan/multi-server
-docker push amitdewangan/multi-worker
+docker build -t amitdewangan/multi-client:latest -t amitdewangan/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t amitdewangan/multi-server:latest -t amitdewangan/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t amitdewangan/multi-worker:latest -t amitdewangan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push amitdewangan/multi-client:latest
+docker push amitdewangan/multi-server:latest
+docker push amitdewangan/multi-worker:latest
+
+docker push amitdewangan/multi-client:$SHA
+docker push amitdewangan/multi-server:$SHA
+docker push amitdewangan/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=amitdewangan/multi-server
+kubectl set image deployments/server-deployment server=amitdewangan/multi-server:$SHA
+kubectl set image deployments/client-deployment client=amitdewangan/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=amitdewangan/multi-worker:$SHA
